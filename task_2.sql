2ff72499d133e645eb073ae45af8b4c479d5ea2a
Kevin.Ogutu <kogutu7@gmail.com>
2025-10-19 16:43:12 +0300

    Task Sql

--- task_2.sql
+++ task_2.sql
@@ -6,7 +6,7 @@ USE alx_book_store;
 -- ===============================
 -- 1. AUTHORS TABLE
 -- ===============================
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS authors (
 -- ===============================
 -- 2. BOOKS TABLE
 -- ===============================
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS books (
 -- ===============================
 -- 3. CUSTOMERS TABLE
 -- ===============================
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS customers (
 -- ===============================
 -- 4. ORDERS TABLE
 -- ===============================
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS orders (
 -- ===============================
 -- 5. ORDER_DETAILS TABLE
 -- ===============================
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
